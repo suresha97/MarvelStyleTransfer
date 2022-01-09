8b990798673fd21fa3be6ace043eb33ccac33782
suresha97 <suresha@tcd.ie>
2022-01-09 12:02:52 +0000

    Initial model

---
+++ src/models/pretrained_vgg_net.py
@@ -0,0 +1,16 @@
+
+import torch
+from torch import Tensor
+
+
+class PretrainedVGGNet:
+    def __init__(self, variant: int) -> None:
+        self._model = torch.hub.load('pytorch/vision:v0.10.0', f'vgg{variant}', pretrained=True).eval()
+
+    def forward(self, input_tensor: Tensor, extraction_layer: int) -> Tensor:
+        if input_tensor.size() != 4:
+            print(f"Tensor of size, {input_tensor.size()}, passed to model did not have batch dimension, "
+                  f"so one has been created.")
+            input_tensor = input_tensor.unsqueeze(0)
+
+        return self._model.features[:extraction_layer](input_tensor).squeeze()
